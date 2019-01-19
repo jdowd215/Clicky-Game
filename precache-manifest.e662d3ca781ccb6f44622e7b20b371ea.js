380002d7c15d21b057927c849de62e41876d5c27
JDowd <jamesddowd@outlook.com>
2019-01-19 11:08:12 -0800

    Updates

---
+++ precache-manifest.e662d3ca781ccb6f44622e7b20b371ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "cdb7d86e4d2b26241d52",
+    "url": "/Clicky-Game/static/js/main.cdb7d86e.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Clicky-Game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "cdb7d86e4d2b26241d52",
+    "url": "/Clicky-Game/static/css/main.5ce1729c.chunk.css"
+  },
+  {
+    "revision": "9746409a34a1bd965c179e29554edef1",
+    "url": "/Clicky-Game/index.html"
+  }
+];
